cdd07ead6c2c4d40621218026f3370c2e72433cc
Raphael <coldinbud@gmail.com>
2019-03-27 06:35:40 +0800

    init project with model

---
+++ HabaneroCodeTest/Models/BaseApiData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HabaneroCodeTest.Models
+{
+    public class BaseApiData
+    {
+        [JsonProperty("BrandId")]
+        public string brandID { get; set; }
+        [JsonProperty("ApiKey")]
+        public string apiKey { get; set; }
+
+        public BaseApiData()
+        {
+            brandID = "0d51cf4c-1d02-e711-80d9-000d3a802d1d";
+            apiKey = "A50C2BDA-4ACF-42E8-AE22-971A9A9F47C7";
+        }
+
+        public BaseApiData(string _brandID, string _apiKey)
+        {
+            brandID = _brandID;
+            apiKey = _apiKey;
+        }
+    }
+}
---
+++ HabaneroCodeTest/Models/GamesData.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HabaneroCodeTest.Models
+{
+    /*
+    {
+        "Games": [
+            {
+                "BrandGameId":"3895b73a-7236-4ff0-b343-d5edfa45bee8",
+                "Name":"5 Lucky Lions",
+                "KeyName":"SG5LuckyLions",
+                "GameTypeId":11,
+                "TranslatedNames":[
+                    {
+                       "LanguageId":1,
+                       "Locale":"en",
+                       "Translation":"5 Lucky Lions"
+                    },
+                    {
+                       "LanguageId":2,
+                       "Locale":"fr-FR",
+                       "Translation":"5 Lucky Lions"
+                    }
+                ]
+            }
+        ]
+    }
+    */
+    public class GamesData
+    {
+        [JsonProperty("Games")]
+        public List<Game> games { get; set; }
+
+        public class Game
+        {
+            [JsonProperty("BrandGameId")]
+            public string brandGameId { get; set; }
+            [JsonProperty("Name")]
+            public string name { get; set; }
+            [JsonProperty("KeyName")]
+            public string keyName { get; set; }
+            [JsonProperty("GameTypeId")]
+            public int gameTypeId { get; set; }
+            [JsonProperty("TranslatedNames")]
+            public List<TranslatedName> translatedNames { get; set; }
+        }
+
+        public class TranslatedName
+        {
+            [JsonProperty("LanguagedId")]
+            public int languagedId { get; set; }
+            [JsonProperty("Locale")]
+            public string locale { get; set; }
+            [JsonProperty("Translation")]
+            public string translation { get; set; }
+        }
+    }
+}
